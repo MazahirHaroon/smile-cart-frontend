7a45a10ae1aff16378ab789f2f8d999ed2f7f9d3
Mazahir Haroon <mazahir.haroon@bigbinary.com>
2025-01-23 19:35:21 +0530

    imported css to the index page

--- src/index.jsx
+++ src/index.jsx
@@ -5,7 +5,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
 import App from "./App";
-import "./App.css";
+import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 
 initializeAxios();
